9d4a339a0927c530699e807bcf7578f617ce2acd
Wong Hoi Sing Edison <hswong3i@gmail.com>
2012-04-06 03:44:39 +0800

    * Update all test cases, counter proved by mysql. * Only test case with mysql passed are keep, else mask.

--- tests/database_test.php
+++ tests/database_test.php
@@ -1,7 +1,7 @@
 <?php
 
 // Initialize configuration in Drupal style.
-require_once dirname(__FILE__) . '/bootstrap.inc';
+require_once dirname(__FILE__) . '/includes.inc';
 
 // Include trimmed version of database_test.test.
 require_once dirname(__FILE__) . '/database_test.test';
--- tests/schema.php
+++ tests/schema.php
@@ -1,7 +1,7 @@
 <?php
 
 // Initialize configuration in Drupal style.
-require_once dirname(__FILE__) . '/bootstrap.inc';
+require_once dirname(__FILE__) . '/includes.inc';
 
 // Include trimmed version of schema.test.
 require_once dirname(__FILE__) . '/schema.test';
