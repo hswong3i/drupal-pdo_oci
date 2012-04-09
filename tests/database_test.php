40054cd576533862568fab43236dd8607bdf3f5d
Wong Hoi Sing Edison <hswong3i@gmail.com>
2012-04-09 12:12:15 +0800

    * Cleanup fake simpletest to prevent potential conflict with core.

--- tests/database_test.php
+++ tests/database_test.php
@@ -1,7 +1,7 @@
 <?php
 
 // Initialize configuration in Drupal style.
-require_once dirname(__FILE__) . '/includes.inc';
+require_once dirname(__FILE__) . '/initialize.inc';
 
 // Include trimmed version of database_test.test.
-require_once dirname(__FILE__) . '/database_test.test';
+require_once dirname(__FILE__) . '/database_test.simpletest';
